513fdc5a0a0dc19954902f413d600b522c5fb3d8
Marlonleon <marlonestebanleonrojas@gmail.com>
2024-08-09 10:56:01 -0500

    cart-firts

---
+++ js/app.js
@@ -0,0 +1,57 @@
+window.addEventListener('keydown',(e)=>{
+if(e.key=='+'){
+incremento()
+}else if(e.key=='-'){
+    decremento() 
+}else if(e.key=='.'){
+    resetear()
+}
+
+})
+
+
+let contador = 0;
+
+const carritos=document.querySelectorAll('.valor')
+const produt=document.getElementById('totel')
+const toCompra=document.getElementById('stock')
+
+// formateamos para que tengan puntos
+
+const producto=parseFloat(toCompra.textContent.replace(/\./g,""))
+const produtformate = producto.toLocaleString('es-CO');
+
+
+
+function incremento(){
+        contador+=1
+        actualizarCarrito()
+}
+
+
+function decremento(){
+
+    if(contador>0){
+        contador-=1
+    }
+
+    actualizarCarrito()
+}
+
+function resetear(){
+    contador = 0;
+    actualizarCarrito()
+}
+
+
+
+// creamos la funcion actualizar para que cada vez que realizaemos una funcion se pinte en el html
+
+function actualizarCarrito(){
+    carritos.forEach(carrito => {
+        carrito.innerHTML = contador;
+    });
+    
+    produt.innerHTML=(contador*producto).toLocaleString("es-CO") ;
+
+}
